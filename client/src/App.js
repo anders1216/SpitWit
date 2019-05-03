10871b92671b4fa70e9f48bd959d4107c5a65562
bbpak <brianbpak@gmail.com>
2019-05-03 09:41:58 -0700

    move game container to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import GameContainer from './containers/GameContainer'
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import NewPlayerForm from '../components/NewPlayerForm'
+import AnswerForm from '../components/AnswerForm'
+import Lobby from '../components/Lobby'
+import DisplayRound from './DisplayRound'
+import './App.css'
 
-const App = () => {
-
-    return (
-      <GameContainer/>
-    );
+class App extends Component {
+	render() {
+		return (
+			<Router>
+				<Route
+					path='/'
+					component={() => <NewPlayerForm player={this.state.player} handleChange={this.handleChange} />}
+				/>
+				<Route
+					path='/lobby'
+					component={() => <Lobby players={this.state.players} player={this.state.player} />}
+				/>
+				<Route
+					path={`/${this.state.game}`}
+					component={() => (
+						<AnswerForm
+							player={this.state.player}
+							prompt={this.state.prompt}
+							handleChange={this.handleChange}
+						/>
+					)}
+				/>
+			</Router>
+		)
+	}
 }
 
-export default App;
+export default App
