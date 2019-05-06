ccfb65b6dc8ce1fb019ace02c121b0f6af8f650c
bbpak <brianbpak@gmail.com>
2019-05-06 11:27:33 -0700

    answer creation

--- server/app/controllers/votes_controller.rb
+++ server/app/controllers/votes_controller.rb
@@ -8,17 +8,6 @@ class VotesController < ApplicationController
     @vote = Vote.create(vote_params)
     render json: @vote
   end
-
-  def update
-    @vote = Vote.find(params[:id])
-    @vote.update(vote_params)
-
-    if @vote.save
-      render json: @vote, status: :accepted
-    else
-      render json: { errors: @vote.errors.full_messages }, status: :unprocessible_entity
-    end
-  end
   
   private
   
