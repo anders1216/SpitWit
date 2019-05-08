afb9f7e8864e2770933d152a2733afbc08ec6705
bbpak <brianbpak@gmail.com>
2019-05-08 10:37:32 -0700

    implement scores

--- server/app/controllers/votes_controller.rb
+++ server/app/controllers/votes_controller.rb
@@ -6,6 +6,8 @@ class VotesController < ApplicationController
 
   def create
     @vote = Vote.create(vote_params)
+    player = Player.find(Answer.find(vote_params[:answer_id]).player_id)
+    player.update(score: player.score + 100)
     render json: @vote
   end
   
